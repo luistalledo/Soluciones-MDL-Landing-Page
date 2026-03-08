3782ac784a39a0fa94b194d354b761928bfc71b0
Luis Talledo <talledoluis924@gmail.com>
2026-03-08 15:12:01 -0500

    Subida de cambios recientes

--- js/main.js
+++ js/main.js
@@ -14,11 +14,10 @@
         const lenis = new Lenis({
             duration: 0.8,
             easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-            direction: 'vertical',
-            smooth: true,
+            smoothWheel: true,      // reemplaza smooth: true
             smoothTouch: false,
             touchMultiplier: 2,
-            normalizeWheel: true,
+            wheelMultiplier: 1,     // controla velocidad del wheel
             infinite: false
         });
 
